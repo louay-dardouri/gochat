fbe9981bf09b14daa65b5df3d3756a451dc8df1d
louay-dardouri <louay.dardouri@insat.ucar.tn>
2025-09-27 14:05:03 +0100

    parser

---
+++ utils/parser.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Command struct {
+	Name string
+	Args []string
+}
+
+const (
+	Prefix  = "/"
+	CmdNick = "nick"
+	CmdSend = "send"
+	CmdView = "view"
+)
+
+func Parse(msg string) (*Command, error) {
+	msg = strings.TrimSpace(msg)
+	if msg == "" {
+		return nil, nil
+	}
+
+	if !strings.HasPrefix(msg, Prefix) {
+		return nil, fmt.Errorf("Prefix missing or unknown")
+	}
+
+	msg = msg[1:]
+	parts := strings.Fields(msg)
+	commandName := strings.ToLower(parts[0])
+	args := parts[1:]
+
+	switch commandName {
+	case CmdNick, CmdSend, CmdView:
+		return &Command{
+			Name: commandName,
+			Args: args,
+		}, nil
+	default:
+		return nil, fmt.Errorf("unknown command %s%s", Prefix, commandName)
+	}
+}
